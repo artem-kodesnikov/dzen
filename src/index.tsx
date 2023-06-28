950987d0924aa74c06a5fcd46a049f0f20a59864
Artem <artiguard@gmail.com>
2023-06-28 13:03:58 +0300

    add hash router

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 
